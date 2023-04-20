e8d4c18a896791a80ee6b081844f498b65ec0847
Gavin-Victor <gavinjvictor@gmail.com>
2023-04-19 18:44:12 -0700

    Changed url to ebay and added price text retrieval.

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -3,7 +3,9 @@ PATH = sys.path
 PATH.append('C:/Users/gavin/OneDrive/Documents/what_its_worth/env/lib/python3.10/site-packages')
 from selenium import webdriver
 
-url = 'https://www.youtube.com/watch?v=CHUxmVVH2AQ&t=164s&ab_channel=Hallden'
+url = 'https://www.ebay.com/sch/i.html?_nkw=INSIGNIA+50-inch+Class+F30+Series+TV&LH_Complete=1&LH_Sold=1&LH_ItemCondition=3000'
 browser = webdriver.Chrome()
+x_path = '//*[@id="srp-river-results"]/ul/li[4]/div/div[2]/div[4]/div[1]/span/span'
 browser.get(url)
-browser.find_element_by_xpath('//*[@id="description-inline-expander"]/yt-attributed-string/span').click()
+price = browser.find_element('xpath', x_path).text
+print(price)
